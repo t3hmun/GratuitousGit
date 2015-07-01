17a337ed3135a39f39ada33f5a12b6b2c6aeb051
mun <t3hmun@gmail.com>
2015-07-01 12:42:19 +0100

    Autocomitting but doesn't ignore git dir (FAIL)

--- gratuitousgit.py
+++ gratuitousgit.py
@@ -1,17 +1,54 @@
-from subprocess import check_call
 import time
+from subprocess import check_call
+from subprocess import CalledProcessError
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
 
 # Configurable paths
-gitpath = 'C:/Program Files (x86)/Git/bin/git.exe'
-repopath = 'W:/zim'
+git_path = 'C:/Program Files (x86)/Git/bin/git.exe'
+repo_path = 'W:/zim'
 
 
-def commit(path):
+class DirModifiedCommitHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        commit()
+
+
+def commit(retry=False):
+    print('## Starting commit.')
     now = str(time.time())
-    addcmd = gitpath + " add -A"
-    commitcmd = gitpath + " commit -a -m 'autocommit:" + now + "'"
-    check_call(addcmd, cwd=path)
-    check_call(commitcmd, cwd=path)
+    addcmd = git_path + " add -A"
+    commit_cmd = git_path + " commit -a -m 'autocommit:" + now + "'"
 
-commit(repopath)
-input('end')
+    try:
+        check_call(addcmd, cwd=repo_path)
+    except CalledProcessError:
+        print('## Add failed.')
+        if not retry:
+            print('## ...retrying.')
+            time.sleep(100)
+            commit(True)
+            return  # The retry will do the commit.
+
+    try:
+        check_call(commit_cmd, cwd=repo_path)
+    except CalledProcessError:
+        print('## Commit failed.')
+        if not retry:
+            print('# ...retrying.')
+            time.sleep(100)
+            commit(True)
+    print('## Commit attempt complete.')
+
+
+def start():
+    ev_handler = DirModifiedCommitHandler()
+    observer = Observer()
+    observer.schedule(ev_handler, repo_path, recursive=True)
+    observer.start()
+    input('hit enter to end')
+    observer.stop()
+
+
+start()
